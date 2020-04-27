21d145b845cef245f809cbfc8cf17782ff0a522f
Mohammad Hasbini <mohammad.hasbini@gmail.com>
2020-04-27 22:12:36 +0300

    change thumb

--- src/processor.py
+++ src/processor.py
@@ -29,7 +29,7 @@ def get_title(video_id):
 
 
 def get_image(video_id):
-    url = f"https://i.ytimg.com/vi/{video_id}/0.jpg"
+    url = f"https://i.ytimg.com/vi/{video_id}/sddefault.jpg"
     response = requests.get(url)
 
     return Image.open(BytesIO(response.content))
